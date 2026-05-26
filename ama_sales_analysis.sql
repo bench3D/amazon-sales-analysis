2690d9ab24a8878050b0057aa881a1cb750a7f18
bench3D <benediktchima@gmail.com>
2026-05-26 17:16:22 +0100

    query analysis

--- ama_sales_analysis.sql
+++ ama_sales_analysis.sql
@@ -22,5 +22,26 @@ LIMIT 5;
 SELECT product_name, rating, discount_percentage
 FROM public.amazon_cleaned
 WHERE rating >= 4.0 AND discount_percentage >= 0.5
+ORDER BY discount_percentage DESC
 LIMIT 10;
 
+--2. Pricing Analysis:
+--What is the average discount percentage by main category?
+SELECT main_category, avg(discount_percentage) as avg_discount
+FROM public.amazon_cleaned
+GROUP BY main_category
+ORDER BY avg_discount DESC;
+
+--Which product has the highest absolute savings (actual_price - discounted_price)?
+SELECT product_name, MAX(column_savings) as savings
+FROM public.amazon_cleaned
+GROUP BY product_name
+ORDER BY savings DESC;
+
+--What is the relationship between discounts and customer ratings?
+SELECT discount_percentage, rating
+FROM public.amazon_cleaned
+WHERE discount_percentage IS NOT NULL AND rating IS NOT NULL;
+
+--How are products distributed across pricing ranges?
+SELECT *
